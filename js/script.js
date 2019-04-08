bd1fc65d63f78444e365396980290ea15e89dc31
Peter White <pwhitedeveloper@gmail.com>
2019-04-08 14:05:16 -0400

    first commit

---
+++ js/script.js
@@ -0,0 +1,16 @@
+// $(document).ready(function(){
+
+// 	var header = $("#myHeader");
+// 	var sticky = header.offset().top;;
+
+// 	window.onscroll = function() {
+
+// 		if (window.pageYOffset > sticky) {
+// 			header.addClass("sticky");
+// 		} else {
+// 			header.removeClass("sticky");
+// 		}
+
+// 	};
+
+// });
