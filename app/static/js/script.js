e64e9db4d1f5e62a503fae0b8e84fe5581a45d7d
Peter White <pwhitedeveloper@gmail.com>
2019-05-03 15:32:12 -0400

    Starting input functionality, TODO: Intergrage more flask operations including logging in

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,16 +1,29 @@
-// $(document).ready(function(){
+$(document).ready(function(){
 
-// 	var header = $("#myHeader");
-// 	var sticky = header.offset().top;;
+  var $path = $("#path");
+  var $command-input = $("#command-input");
 
-// 	window.onscroll = function() {
+  function changePath(path) {
+    return path;
+  }
 
-// 		if (window.pageYOffset > sticky) {
-// 			header.addClass("sticky");
-// 		} else {
-// 			header.removeClass("sticky");
-// 		}
+  var commands = {
+    "cd": "Change directory",
+    "run": "Run application",
+    "help": "Get function names",
+    "stuff": "See all files in directory",
+    "nav": "Open program navigation menu",
+    "login": "Log in to your Personal Computer account",
+    "logout": "Log out of your Personal Computer account",
+    "register": "Create a Personal Computer account to log in and log out of",
+    "cleanse": "Purge all items on screen from existence",
+    "getmeouttahere": "Quit"
+  };
 
-// 	};
+  $command-input.on("keypress", function(e){
+    if(e.which == 13) {
+      console.log(commands[$(this).val()]);
+    }
+  });
 
-// });
+});
