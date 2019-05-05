5582b38d04ca201fb14baef998df87880a9e4756
Peter White <pwhitedeveloper@gmail.com>
2019-05-05 18:59:50 -0400

    TODO: Fix the generate form

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,10 +1,27 @@
 $(document).ready(function(){
 
+  var $command = $(".command");
+  var $command_input = $("input#command-input");
+  var $dosBoxx = $("div#dosBoxx");
   var $path = $("#path");
-  var $command-input = $("#command-input");
 
   function changePath(path) {
     return path;
+  }
+
+  function oldInput(path, command) {
+
+    var html = '<div class="row cell">';
+        html += '<div class="col-md-12">';
+        html += '<div class="input-group mb-3">';
+        html += '<div class="input-group-prepend">';
+        html += '<span class="input-group-text" id="path">' + path + '</span>';
+        html += '</div>';
+        html += '<input type="text" class="form-control" id="command-input" placeholder="First name" aria-describedby="basic-addon3" disabled>';
+        html += '</div></div></div>'
+
+        $dosBoxx.append(html);
+        $command_input = $("input#command-input").last();
   }
 
   var commands = {
@@ -17,13 +34,18 @@ $(document).ready(function(){
     "logout": "Log out of your Personal Computer account",
     "register": "Create a Personal Computer account to log in and log out of",
     "cleanse": "Purge all items on screen from existence",
-    "getmeouttahere": "Quit"
+    "getmethehellouttahere": "Quit"
   };
 
-  $command-input.on("keypress", function(e){
+  $command_input.on("keypress", function(e){
     if(e.which == 13) {
-      console.log(commands[$(this).val()]);
+        var today = new Date();
+        console.log(today);
+        oldInput($(this).parent().children("div.input-group-prepend").children("#path").text(), $(this).val());
+        $(this).val() = "";
     }
   });
+
+
 
 });
