43f841f5233a2dc5509548f39f79e7f415b3a62d
Peter White <pwhitedeveloper@gmail.com>
2019-05-08 20:20:15 -0400

    user model / login and registeration form

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
 from app import app, db, login
 from flask import render_template, url_for, redirect, flash
+from app.forms import CommandInput, LoginForm, RegisterForm
 from app.models import User
 from flask_login import current_user, login_user, logout_user, login_required
 
